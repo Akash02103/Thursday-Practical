55710aa08edc609660b80fafce0176003d803a59
Akash02103 <128777251+Akash02103@users.noreply.github.com>
2023-03-30 13:39:33 +0530

    Update Rectangle.java

--- 30-03-2023/Rectangle.java
+++ 30-03-2023/Rectangle.java
@@ -6,33 +6,40 @@ Its constructor having parameters for length and breadth is used to initialize l
 Print the area and perimeter of a rectangle
 */
 
-class Rectangle
-{
-    int length,breadth;
-    Rectangle(int length,int breadth)
-    {
-        this.length=length;
-        this.breadth=breadth;
+// A class to represent a Rectangle
+class Rectangle {
+    int length, breadth;
+
+    // Constructor to initialize length and breadth of rectangle
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
     }
 
-    void cal_area()
-    {
-        int area=length*breadth;
-        System.out.println("Area of rectangle is:"+area);
+    // Method to calculate area of rectangle
+    void cal_area() {
+        int area = length * breadth;
+        System.out.println("Area of rectangle is: " + area);
     }
 
-    void cal_perimeter()
-    {
-        int perimeter=2*(length+breadth);
-        System.out.println("perimeter of Rectangle is:"+perimeter);
+    // Method to calculate perimeter of rectangle
+    void cal_perimeter() {
+        int perimeter = 2 * (length + breadth);
+        System.out.println("Perimeter of Rectangle is: " + perimeter);
     }
 
-    public static void main(String args[])
-    {
-        Rectangle re=new Rectangle(5,10);
+ 
+    public static void main(String args[]) {
+        // Create an instance of Rectangle with length 5 and breadth 10
+        Rectangle re = new Rectangle(5, 10);
+
+        // Call the cal_area method to print area of the rectangle
         re.cal_area();
+
+        // Call the cal_perimeter method to print perimeter of the rectangle
         re.cal_perimeter();
     }
-    
+}
+
 
 }
